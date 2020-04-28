143fdd237a7d492ad87d1a814c1c8792dd0bd14a
HBertram <435832733@qq.com>
2020-04-28 18:25:58 +0800

    1

---
+++ components/bert-tag-group/bert-tag-group.vue
@@ -0,0 +1,67 @@
+<template>
+  <view>
+	<uni-section v-show="showSection" :title="title" type="line"></uni-section>
+    <checkbox-group class="content" @change="checkboxChange">
+		<template v-for="(item,index) in labelList">
+			<label @click="labelBtn(item.value,index)" :key="item.value" :class="item.checked ?  'checkbox selectBox' : 'checkbox '">  
+				<checkbox :value="item.value" :checked="item.checked" v-show="false"/>{{item.name}}
+			</label>
+		</template>
+    </checkbox-group>
+  </view>
+</template>
+
+<script>
+  export default {
+	  props: {
+		  showSection: {
+			  type: Boolean,
+			  required: false,
+			  default: true
+		  },
+		  title: {
+			  type: String,
+			  required: false,
+			  default: "标签"
+		  },
+		  labelList: {
+			  type: Array,
+			  default: [],
+			  required: true
+		  }
+	  },
+    data() {
+      return {
+        labelName:''
+      };
+    },
+    methods:{
+      labelBtn(name,index){
+        this.labelName = name
+	    this.labelList[index].checked = this.labelDataList.includes(name)
+      },
+      checkboxChange: function (e) {
+        this.labelDataList = e.detail.value
+      },
+    }
+  }
+</script>
+
+<style>
+  .selectBox{
+    background: #EB5248!important;
+    color: #fff!important;
+  }
+  .checkbox{
+	display: inline-flex;
+	flex-wrap:wrap;
+    padding: 5px 10px;
+    border: 1px solid #EB5248;
+    margin: 4px;
+    border-radius: 7upx;
+    color: #000;
+  }
+  .content{
+    padding: 10px;
+  }
+</style>
--- pages/tabbar/tabbar-1/tabbar-1.vue
+++ pages/tabbar/tabbar-1/tabbar-1.vue
@@ -1,25 +1,88 @@
 <template>
-	<view class="content">
-		页面 - 1
+	<view class="container">
+		<view class="bghead"></view>
+		<view class="top">
+			<view class="top-title">
+				asdasd
+			</view>
+			<view class="top-content">
+				<view class="top-box">
+					asdasd
+				</view>
+			</view>
+		</view>
+		<view class="main">
+			<bert-tag-group title="生活" :labelList="labelList"></bert-tag-group>
+			<bert-tag-group title="学习" :labelList="labelList"></bert-tag-group>
+			<bert-tag-group title="工作" :labelList="labelList"></bert-tag-group>
+			<bert-tag-group title="交际" :labelList="labelList"></bert-tag-group>
+		</view>
 	</view>
 </template>
 
 <script>
-export default {
-	data() {
-		return {
-			title: 'Hello'
-		};
-	},
-	onLoad() {},
-	methods: {}
-};
+	import uniSection from "@/components/uni-section/uni-section.vue"
+	import bertTagGroup from "@/components/bert-tag-group/bert-tag-group.vue"
+  export default {
+    data() {
+      return {
+		  labelList: [
+			{
+				name: '吃饭',
+				value: '1',
+				checked: false
+			},
+			{
+				name: '睡觉',
+				value: '2',
+				checked: false
+			},
+			{
+				name: '瘦了',
+				value: '3',
+				checked: false
+			},
+			{
+				name: '吃大餐',
+				value: '4',
+				checked: false
+			},
+			{
+				name: '做运动',
+				value: '5',
+				checked: false
+			}
+		  ]
+	  }
+    },
+    methods:{
+    }
+  }
 </script>
 
 <style>
-.content {
-	text-align: center;
-	height: 400upx;
-	margin-top: 200upx;
-}
+	.bghead{
+		position: absolute;
+		left: 0;
+		top: 0;
+		height: 300rpx;
+		width: 100%;
+		background-color: #66ccff;
+		z-index: -1;
+	}	
+	.top {
+		flex-direction: column;
+		.top-content {
+			display: flex;
+			flex-direction: row;
+			justify-content: space-around;	
+			
+			.top-box {
+				width: 700upx;
+				height: 220upx;
+				background-color: #ffffff;
+				border-radius: 15upx;
+			}
+		}
+	}
 </style>
